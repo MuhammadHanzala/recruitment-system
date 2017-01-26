81f22c6e8a45bf9364e43c4db98862a64753771f
adnanirfan <adnanshurta@gmail.com>
2017-01-26 19:32:44 +0500

    updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CompanyComponent } from './company/company.component';
+import { LoggedInGuardService } from './logged-in-guard.service';
 
 const routes: Routes = [
   {
@@ -12,8 +18,21 @@ const routes: Routes = [
     path: 'signup', component: SignupComponent, children: []
   },
   {
+    path: 'student', component: DashboardComponent, children: [],
+    canActivate: [LoggedInGuardService]
+  },
+  {
+    path: 'company', component: CompanyComponent, children: [],
+    canActivate: [LoggedInGuardService]
+  },
+  {
+    path: 'admin', component: AdminComponent, children: [],
+    canActivate: [LoggedInGuardService]
+  },
+  {
     path: '', redirectTo: 'login', pathMatch: 'full'
-  }
+  },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
