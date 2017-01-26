e844971d6152500b1b240054dccee8904bb0cf93
adnanirfan <adnanshurta@gmail.com>
2017-01-26 19:27:27 +0500

    created dashboard for student

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  jobs$: any;
+  job: any = {
+    title: '',
+    name: '',
+    positions: '',
+    postedat: '',
+    description: ''
+  };
+
+  constructor(private fb: AngularFire) {
+    fb.database.list('/jobs')
+      .subscribe(data => {
+        console.log(data);
+        this.jobs$ = data;
+      });
+  }
 
   ngOnInit() {
   }
+
+  apply(job) {
+    console.log(job);
+    this.job = job;
+  }
+
+
 
 }
